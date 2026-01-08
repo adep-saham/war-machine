b3a60504dca9c28693e42fb9bccfaf0dca679f80
adep-saham <best.adeprasetyo@gmail.com>
2026-01-08 13:53:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 import streamlit as st
-from ui.master_guardrail import render_master
-from ui.price_input import render_price_input
-from ui.market_share_data import render_market_share_upload
+from ui.upload_data import render_upload_data
 from ui.war_room import render_war_room
-from ui.explorer import render_explorer
 
 st.set_page_config(page_title="Gold War Room", layout="wide")
 
-tabs = st.tabs([
-    "Master & Guardrail",
-    "Input Harga",
-    "Market Share Data",
-    "War Room",
-    "Explorer"
-])
+tabs = st.tabs(["Upload Data", "War Room"])
 
-with tabs[0]: render_master()
-with tabs[1]: render_price_input()
-with tabs[2]: render_market_share_upload()
-with tabs[3]: render_war_room()
-with tabs[4]: render_explorer()
+with tabs[0]:
+    render_upload_data()
+
+with tabs[1]:
+    render_war_room()
